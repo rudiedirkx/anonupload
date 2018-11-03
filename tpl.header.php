d7a19517507912b0d26d17d9afdd4882c3cae7e2
Rudie Dirkx <github@hotblocks.nl>
2018-11-03 17:02:50 +0100

    +theme color

--- tpl.header.php
+++ tpl.header.php
@@ -2,9 +2,10 @@
 <html>
 
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1" />
-<meta charset="utf-8" />
-<title>Anonymous upload</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="theme-color" content="#333" />
+	<meta charset="utf-8" />
+	<title>Anonymous upload</title>
 </head>
 
 <body>
