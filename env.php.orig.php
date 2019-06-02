14ba5c1f3dd3992c3d41f55a4f26309adc961e47
Rudie Dirkx <github@hotblocks.nl>
2019-06-02 22:25:04 +0200

    config+

--- env.php.orig.php
+++ env.php.orig.php
@@ -1,3 +1,5 @@
 <?php
 
-define('ANONUPLOAD_FILES_DIR', __DIR__ . '/../uploads');
+const ANONUPLOAD_PASSWORDS = ['$2y$10$x58VtzU9XGM8xtcpGwqF/.OnyO0HB1NDPNs2FMTgR13PX/Nod2.UO']; // test
+const ANONUPLOAD_DB_DIR = __DIR__ . '/db';
+const ANONUPLOAD_FILES_DIR = __DIR__ . '/../uploads';
--- inc.bootstrap.php
+++ inc.bootstrap.php
@@ -3,8 +3,6 @@
 require 'env.php';
 require 'vendor/autoload.php';
 
-define('ANONUPLOAD_DB_DIR', __DIR__ . '/db');
-
 $db = db_sqlite::open(array('database' => ANONUPLOAD_DB_DIR . '/files.sqlite3'));
 
 $db->ensureSchema(require 'inc.db-schema.php');
